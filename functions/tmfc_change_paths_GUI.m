07eb862e48f7e0654eb1e47c52013b22f2451fe5
Masharipov <masharipov@ihb.spb.ru>
2024-09-25 16:59:59 +0300

    Update tmfc_change_paths_GUI.m

--- functions/tmfc_change_paths_GUI.m
+++ functions/tmfc_change_paths_GUI.m
@@ -32,85 +32,54 @@ function tmfc_change_paths_GUI(paths)
 %
 % Contact email: masharipov@ihb.spb.ru
 
+tmfc_CP_MW = figure('Name', 'Change paths', 'NumberTitle', 'off', 'Units', 'normalized', 'Position', [0.30 0.40 0.35 0.26],'Resize','off','color','w','MenuBar', 'none','ToolBar', 'none');
 
-%Creation of BASE OUTLINE for Window                                  
-CP_1 = figure('Name', 'Change paths', 'NumberTitle', 'off', 'Units', 'normalized', 'Position', [0.30 0.40 0.35 0.26],'Resize','off','color','w','MenuBar', 'none','ToolBar', 'none');
+tmfc_CP_MW_S1 = uicontrol(tmfc_CP_MW,'Style','text','String', 'Change paths in SPM.mat files','Units', 'normalized','fontunits','normalized', 'fontSize', 0.20, 'Position', [0.18 0.66 0.660 0.300],'backgroundcolor',get(tmfc_CP_MW,'color'));
+tmfc_CP_MW_S2 = uicontrol(tmfc_CP_MW,'Style','text','String', 'Old pattern (e.g., C:\Project_folder\Subjects):','Units', 'normalized','fontunits','normalized', 'fontSize', 0.20, 'Position', [0.05 0.55 0.450 0.260],'backgroundcolor',get(tmfc_CP_MW,'color'));
+tmfc_CP_MW_E1 = uicontrol(tmfc_CP_MW,'Style','edit','String', '','Units', 'normalized','fontunits','normalized', 'fontSize', 0.50,'HorizontalAlignment', 'left', 'Position', [0.05 0.60 0.880 0.110]);
 
-% Initializing Elements of the UI
-CP_T1 = uicontrol(CP_1,'Style','text','String', 'Change paths in SPM.mat files','Units', 'normalized','fontunits','normalized', 'fontSize', 0.20);
+tmfc_CP_MW_S3 = uicontrol(tmfc_CP_MW,'Style','text','String', 'New pattern (e.g., E:\All_Projects\Project_folder\Subjects):','Units', 'normalized','fontunits','normalized', 'fontSize', 0.20, 'Position', [0.048 0.29 0.590 0.260],'backgroundcolor',get(tmfc_CP_MW,'color'));
+tmfc_CP_MW_E2 = uicontrol(tmfc_CP_MW,'Style','edit','String', '','Units', 'normalized','fontunits','normalized', 'fontSize', 0.50,'HorizontalAlignment', 'left', 'Position', [0.05 0.35 0.880 0.110]);
 
-CP_T2_Q = uicontrol(CP_1,'Style','text','String', 'Old pattern (e.g., C:\Project_folder\Subjects):','Units', 'normalized','fontunits','normalized', 'fontSize', 0.20);
-CP_T2_A = uicontrol(CP_1,'Style','edit','String', '','Units', 'normalized','fontunits','normalized', 'fontSize', 0.50,'HorizontalAlignment', 'left');
+tmfc_CP_MW_S4 = uicontrol(tmfc_CP_MW,'Style','text','String', 'Backups of original SPM.mat files are made with the ''.old'' suffix','Units', 'normalized','fontunits','normalized', 'fontSize', 0.20, 'Position', [0.048 0.06 0.640 0.260],'backgroundcolor',get(tmfc_CP_MW,'color'));
 
-CP_T3_Q = uicontrol(CP_1,'Style','text','String', 'New pattern (e.g., E:\All_Projects\Project_folder\Subjects):','Units', 'normalized','fontunits','normalized', 'fontSize', 0.20);
-CP_T3_A = uicontrol(CP_1,'Style','edit','String', '','Units', 'normalized','fontunits','normalized', 'fontSize', 0.50,'HorizontalAlignment', 'left');
-
-CP_T4 = uicontrol(CP_1,'Style','text','String', 'Backups of original SPM.mat files are made with the ''.old'' suffix','Units', 'normalized','fontunits','normalized', 'fontSize', 0.20);
-
-CP_OK = uicontrol(CP_1,'Style','pushbutton', 'String', 'OK','Units', 'normalized','fontunits','normalized', 'fontSize', 0.35);
-CP_Help = uicontrol(CP_1,'Style','pushbutton', 'String', 'Help','Units', 'normalized','fontunits','normalized', 'fontSize', 0.35);
-
-% Assigning Positions of elements
-CP_T1.Position = [0.18 0.66 0.660 0.300];
-CP_T2_Q.Position = [0.05 0.55 0.450 0.260];
-CP_T2_A.Position = [0.05 0.60 0.880 0.110];
-CP_T3_Q.Position = [0.048 0.29 0.590 0.260];
-CP_T3_A.Position = [0.05 0.35 0.880 0.110];
-CP_T4.Position = [0.048 0.06 0.640 0.260];   
-CP_OK.Position = [0.26 0.05 0.180 0.130];
-CP_Help.Position = [0.54 0.05 0.180 0.130];
-
-set([CP_T1,CP_T2_Q,CP_T3_Q,CP_T4],'backgroundcolor',get(CP_1,'color'));
-
-% Assigning Functions Callbacks for each Element (button, listbox etc)
-set(CP_OK, 'callback', @execute_change)
-set(CP_Help, 'callback', @help_details)
-
+tmfc_CP_MW_OK = uicontrol(tmfc_CP_MW,'Style','pushbutton', 'String', 'OK','Units', 'normalized','fontunits','normalized', 'fontSize', 0.35, 'Position', [0.26 0.05 0.180 0.130],'callback', @execute_change);
+tmfc_CP_MW_Help = uicontrol(tmfc_CP_MW,'Style','pushbutton', 'String', 'Help','Units', 'normalized','fontunits','normalized', 'fontSize', 0.35, 'Position', [0.54 0.05 0.180 0.130],'callback', @help_window);
+movegui(tmfc_CP_MW,'center');
 
 function execute_change(~,~)
-    oldp = get(CP_T2_A, 'String');
-    newp = get(CP_T3_A, 'String');
+    old_path = get(tmfc_CP_MW_E1, 'String');
+    new_path = get(tmfc_CP_MW_E2, 'String');
     try   
-        spm_changepath(char(paths),char(oldp),char(newp));        
+        spm_changepath(char(paths),char(old_path),char(new_path));        
         disp('Paths have been changed.');
     catch 
         disp('Paths have not been changed.');
     end
-    close(CP_1);
+    close(tmfc_CP_MW);
 end
 
 
-function help_details(~,~)
-    CP_H = figure('Name', 'Change paths: Help', 'NumberTitle', 'off', 'Units', 'normalized', 'Position', [0.60 0.40 0.35 0.36],'Resize','off','MenuBar', 'none','ToolBar', 'none','color','w'); %X Y W H
+function help_window(~,~)
+    tmfc_CP_HW = figure('Name', 'Change paths: Help', 'NumberTitle', 'off', 'Units', 'normalized', 'Position', [0.60 0.40 0.35 0.45],'Resize','off','MenuBar', 'none','ToolBar', 'none','color','w');
 
-    THE_DETAILS = {'Suppose you moved the project folder after fist-level model specification and/or estimation.','',...
+    help_S1 = {'Suppose you moved the project folder after fist-level model specification and/or estimation.','',...
         'Original SPM.mat file contains old paths to the model directory, functional images, etc.',...
         '',...
         'The TMFC toolbox uses paths recorded in SPM.mat files. To get access to functional files, you need to change paths in SPM.mat files.'};
-    THE_DETAILS_3 = {'Old pattern: C:\Project_folder\Subjects','New pattern: E:\All_Projects\Project_folder\Subjects','',...
+    help_S3 = {'Old pattern: C:\Project_folder\Subjects','New pattern: E:\All_Projects\Project_folder\Subjects','',...
         'Old path for the first functional image: ','C:\Project_folder\Subjects\Sub_01\func\swar_001.nii','',...
         'New path for the first functional image: ','E:\All_Projects\Project_folder\Subjects\Sub_01\func\swar_001.nii'};
 
-
-    CP_DT_1 = uicontrol(CP_H,'Style','text','String', THE_DETAILS,'Units', 'normalized', 'HorizontalAlignment', 'left','fontunits','normalized', 'fontSize', 0.10);
-    CP_DT_2 = uicontrol(CP_H,'Style','text','String', 'Example:','Units', 'normalized', 'HorizontalAlignment', 'left','fontunits','normalized', 'fontSize', 0.40,'fontweight', 'bold');
-    CP_DT_3 = uicontrol(CP_H,'Style','text','String', THE_DETAILS_3,'Units', 'normalized', 'HorizontalAlignment', 'left','fontunits','normalized', 'fontSize', 0.10);
-    CP_H_OK = uicontrol(CP_H,'Style','pushbutton', 'String', 'OK','Units', 'normalized','fontunits','normalized', 'fontSize', 0.35);
-
-    set([CP_DT_1,CP_DT_2,CP_DT_3],'backgroundcolor',get(CP_H,'color'));
-    % X Y W H
-    CP_DT_1.Position = [0.04 0.56 0.900 0.400];
-    CP_DT_2.Position = [0.04 0.46 0.200 0.100];
-    CP_DT_3.Position = [0.04 0.06 0.900 0.400];
-    CP_H_OK.Position = [0.80 0.08 0.150 0.090];
+    tmfc_CP_HW_S1 = uicontrol(tmfc_CP_HW,'Style','text','String', help_S1,'Units', 'normalized', 'HorizontalAlignment', 'left','fontunits','normalized', 'fontSize', 0.09,'backgroundcolor',get(tmfc_CP_HW,'color'), 'Position', [0.04 0.56 0.900 0.400]);
+    tmfc_CP_HW_S2 = uicontrol(tmfc_CP_HW,'Style','text','String', 'Example:','Units', 'normalized', 'HorizontalAlignment', 'left','fontunits','normalized', 'fontSize', 0.35,'fontweight', 'bold','backgroundcolor',get(tmfc_CP_HW,'color'), 'Position', [0.04 0.48 0.200 0.100]);
+    tmfc_CP_HW_S3 = uicontrol(tmfc_CP_HW,'Style','text','String', help_S3,'Units', 'normalized', 'HorizontalAlignment', 'left','fontunits','normalized', 'fontSize', 0.09,'backgroundcolor',get(tmfc_CP_HW,'color'), 'Position', [0.04 0.08 0.900 0.400]);
+    tmfc_CP_HW_OK = uicontrol(tmfc_CP_HW,'Style','pushbutton', 'String', 'OK','Units', 'normalized','fontunits','normalized', 'fontSize', 0.35, 'Position', [0.45 0.04 0.140 0.090],'callback', @close_tmfc_CP_HW);
+    movegui(tmfc_CP_HW,'center');
     
-    set(CP_H_OK, 'callback', @CLOSE_CP_H_OK);
-    
-    function CLOSE_CP_H_OK(~,~)
-        close(CP_H);
+    function close_tmfc_CP_HW(~,~)
+        close(tmfc_CP_HW);
     end
 end
-
-uiwait(CP_1);
-    
+uiwait(tmfc_CP_MW);
 end
